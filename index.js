2a5066f750a48d82c5823e2706e8913082dc5164
Трофимов Денис <mail@denistrofimov.com>
2016-02-12 17:06:46 +0500

    migrate to method based routing

--- index.js
+++ index.js
@@ -17,25 +17,6 @@ module.exports = function (base) {
     return bootloader.load({
         base: base
     }).then(function (core) {
-
-        // catch 404 and forward to error handler
-        core.app.use(function (req, res, next) {
-            next(core.service('utils').error('Не найдено', 404));
-        });
-
-        // error handlers
-        core.app.use(function (err, req, res, next) {
-            var message = err.message || "Internal server error";
-            var status = err.status || 500;
-            res.status(status);
-            res.send({
-                code: status,
-                message: message,
-                url: req.url
-            });
-            console.error(err.stack);
-        });
-
         return http(core.app, {
             port: core.config.server.port
         }).then(function () {
